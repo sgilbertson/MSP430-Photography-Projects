ef05e1164b95457449d760ad42b38723062506b9
sgilbertson <gilbertson.scott@gmail.com>
2015-08-23 18:32:08 -0400

    Stepper 1: Initial implementation of bipolar output to L293D or similar driver chip using 4 output pins, with the A+ and B+ states reflected on the LEDs.

--- Stepper1/main.c
+++ Stepper1/main.c
@@ -1,16 +1,42 @@
 #include "msp430G2553.h"
 
-/* PWM inverter to drive 24V AC motor for time-lapse rig
+/* PWM inverter to drive stepper motor for a motion time-lapse rig
  *
- * Connections:
+ * Method:
+ * Periodic interrupt occurs at four times the full step rate.
+ * ISR toggles output pins to make two squarewaves 90° out of phase.
+ *
+ * Stepper motor connections:
+ *  - P2.0 = A+
+ *  - P2.1 = A-
+ *  - P2.5 = B+
+ *  - P2.3 = B-
+ *
+ * FUTURE:
+ *  - Buttons to control start/stop and direction
+ *  - Speed potentiometer on analog input A1
+ *
+ * Connections left alone from PWM project (for now):
  *  - P2.2 drives one MOSFET
  *  - P2.4 drives the other MOSFET
  *  - P1.0 LED 1 (same as on Launchpad)
  *  - P1.6 LED 2 (same as on Launchpad)
  */
 
+// Stepper motor connection definitions
+#define STEPPER_A1 BIT0
+#define STEPPER_A0 BIT1
+#define STEPPER_B1 BIT5
+#define STEPPER_B0 BIT3
+#define STEPPER_ALL_BITS (STEPPER_A1 + STEPPER_A0 + STEPPER_B1 + STEPPER_B0)
+#define STEPPER_DIR P2DIR
+#define STEPPER_OUT P1OUT
+
+
+// LEDs
 #define     LED1                  BIT0
 #define     LED2                  BIT6
+#define     LED_ALL               (LED1|LED2)
 #define     LED_DIR               P1DIR
 #define     LED_OUT               P1OUT
 
@@ -21,13 +47,16 @@
 #define PWM_75_PERCENT ((PWM_PERIOD*3)/4)
 
 short irqcount = 0;
-
+volatile unsigned short step_interval = APPROX_CLOCK_HZ / 10; // start at 10Hz, which is 2.5 steps per second
+volatile unsigned short stepperBits = STEPPER_A1 | STEPPER_B1;
 void main(void)
 {
 	WDTCTL = WDTPW + WDTHOLD;  // Stop WDT
 
-	LED_DIR |= LED1 + LED2;
-	LED_OUT &= ~(LED1 + LED2);
+	LED_DIR |= LED_ALL;
+	LED_OUT &= ~LED_ALL;
+	STEPPER_DIR |= STEPPER_ALL_BITS;
+	STEPPER_OUT &= ~STEPPER_ALL_BITS;
 
 	// Set up a timer interrupt
 //	BCSCTL1 |= DIVA_1;                        // ACLK/2
@@ -62,12 +91,30 @@ void main(void)
 __interrupt void Timer_A0 (void)
 {
 	irqcount++;
-	LED_OUT ^= ((irqcount&4)?LED1:0) | ((irqcount&6)?LED2:0);
+	switch(irqcount&3)
+	{
+	case 0:
+		stepperBits = STEPPER_A1 | STEPPER_B0;
+		break;
+	case 1:
+		stepperBits = STEPPER_A1 | STEPPER_B1;
+		break;
+	case 2:
+		stepperBits = STEPPER_A0 | STEPPER_B1;
+		break;
+	default:
+	case 3:
+		stepperBits = STEPPER_A0 | STEPPER_B0;
+		break;
+	}
+	STEPPER_OUT = (STEPPER_OUT & ~STEPPER_ALL_BITS) | stepperBits;
+	// LEDs indicate the A+ and B+ stepper signal states
+	LED_OUT = (LED_OUT&~LED_ALL) | ((stepperBits&STEPPER_A1)?LED1:0) | ((stepperBits&STEPPER_B1)?LED2:0);
 	/*
 	if (LED_OUT | LED1)
 		LED_OUT = (LED_OUT ^ LED2) && ~LED1;
 	else
 		LED_OUT |= LED1;
 	*/
-	TA0CCR0 += 50000;                            // Add Offset to CCR0
+	TA0CCR0 += step_interval;                            // Add Offset to CCR0
 }
